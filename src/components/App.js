eae746fe988effd2c87c1e2c46279a9060eb5fa9
kimjinu <kimjinu5555@gmail.com>
2018-02-03 01:27:25 +0900

    Hello
    이상한 초기화면 Hello로 수정!

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
 
 class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: ''
-    };
-  }
   render() {
     return (
-      <div>
-        <button onClick={() => {this.setState({name: 'KakaoTocs'});}}>Click</button>
-        <h1>Hello React TFS {this.state.name}</h1>
-      </div>
+      <h1>Hello</h1>
     );
   }
 }
